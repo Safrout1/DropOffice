536fab26a8fcc4ab1649f5bb06cf2e72d7af34aa
Safrout1 <khalid.abdulnasser1@gmail.com>
2015-04-05 13:18:23 +0200

    extracted the test editor

---
+++ text editor/app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.order("created_at DESC")
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path, notice: "The article has been successfully created."
+    else
+      render action: "new"
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      redirect_to articles_path, notice: "The article has been successfully updated."
+    else
+      render action: "edit"
+    end
+  end
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
